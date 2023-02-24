ac534f99e69a2304ed1fc7fb6a0520c6120b22da
shubhampachghare <shubhampachghare@gmail.com>
2023-02-24 17:22:22 +0530

    [Shubham]-PerfectNumber

---
+++ PerfectNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PerfectNumber
+{
+    public class PerfectNumber1
+    {
+        public void number()
+        {
+            int i, n, sum = 0;
+            Console.WriteLine("Enter a number:  ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i < n; i++)
+            {
+
+                if (n % i == 0)
+                {
+                    sum = sum + i;
+                    Console.WriteLine("Sum is:  " + sum);
+                }
+            }
+
+
+            if (n == sum)
+            {
+                Console.WriteLine(" Perfect number:  " + n);
+            }
+            else
+
+                Console.WriteLine("Not Perfect number");
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
 ﻿using Fibonacci_Series;
+using PerfectNumber;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        
-        FibonacciSeries obj = new FibonacciSeries();
-        obj.FibonacciSeries1();
-
+        PerfectNumber1 obj1 = new PerfectNumber1();
+        obj1.number();
     }
 }
