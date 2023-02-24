db78d858156d01806f46b62f9a95f83c7c8e6271
shubhampachghare <shubhampachghare@gmail.com>
2023-02-24 17:33:01 +0530

    [Shubham]-ReverseNumber

---
+++ ReverseNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReverseNumber
+{
+    public class ReverseNumber1
+    {
+        public void CheckPalindrome()
+        {
+            int sum = 0;
+            int temp = 0;
+            int remainder;
+            Console.WriteLine("Enter the number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            while (number != 0)
+            {
+                remainder = number % 10;
+                sum = sum * 10 + remainder;
+                number = number / 10;
+            }
+
+
+            Console.WriteLine("Reverse number is:   " + sum);
+        }
+    }
+}
