b32c9612ff6c8fb62ae0958cedd356ca62ad4fa8
shubhampachghare <shubhampachghare@gmail.com>
2023-02-24 17:07:30 +0530

    [Shubham]-FibonacciSeries

---
+++ FibonacciSeries.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Series
+{
+    public class FibonacciSeries
+    {
+        public void FibonacciSeries1()
+        {
+
+            {
+                int a = 0, b = 1, c = 0, len;
+
+                Console.Write("length:  ");
+                len = Convert.ToInt32(Console.ReadLine());
+                Console.Write("{0} {1} ", a, b);
+                for (int i = 2; i < len; i++)
+                {
+                    c = a + b;
+                    Console.Write("{0} ", c);
+                    a = b;
+                    b = c;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Fibonacci_Series;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        FibonacciSeries obj = new FibonacciSeries();
+        obj.FibonacciSeries1();
+
+    }
+}
